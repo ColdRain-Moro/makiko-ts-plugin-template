09da2658cfb43ae5483ebe13f52eefdbd052343e
Rain <69996135+ColdRain-Moro@users.noreply.github.com>
2022-08-25 00:10:12 +0800

    :bug: use this instead of globalThis

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import { GroupMessageEvent, Listener, subscribeEvent } from "makiko-api"
 let listener: Listener
 
 /**
+ * 
  * 插件加载
  */
 // @ts-ignore
-globalThis.onLoad = () => {
-    console.log("Hello World");
+this.onLoad = () => {
     listener = subscribeEvent<GroupMessageEvent>(GroupMessageEvent, (e) => {
         if (e.getMessage().contentToString() == "3G") {
             e.getGroup().sendMessage("重现3g荣光, 我辈义不容辞！")
@@ -19,7 +19,6 @@ globalThis.onLoad = () => {
  * 插件卸载
  */
 // @ts-ignore
-globalThis.onUnload = () => {
-    console.log("Goodbye World");
+this.onUnload = () => {
     listener.complete()
 }
